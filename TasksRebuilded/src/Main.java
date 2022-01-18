a5b309694b56d10913b592ffc847190dc00fe158
maksaid <mapinchuk12@gmailcom>
2022-01-18 21:53:11 +0300

    try

---
+++ TasksRebuilded/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CommandLineRunner runner = new CommandLineRunner();
+        runner.workingprocess();
+
+    }
+}
